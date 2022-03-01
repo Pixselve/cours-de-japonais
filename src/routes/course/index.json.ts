711414ebb95cc48c1b66b1cb28474e7eb6b392bf
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-03-01 20:59:08 +0100

    feat: quick home page

---
+++ src/routes/course/index.json.ts
@@ -0,0 +1,11 @@
+import getCourses from "../../utils/getCourses";
+
+export async function get() {
+
+  const courses = await getCourses();
+
+
+  return {
+    body: JSON.stringify(courses)
+  };
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold">Liste des cours WIP 👷‍</h1>
+
+<ol class="list-decimal">
+  {#each courses as course}
+    <li>
+      {course.title}
+      <ul class="ml-5">
+        {#each course.chapters as chapter}
+          <li>
+            {chapter.title}
+            <ul class="ml-5">
+              {#each chapter.children as child}
+                <li><a class="text-blue-600 hover:underline" href={`course${child.path}`}>{child.title}</a> </li>
+              {/each}
+            </ul>
+          </li>
+        {/each}
+      </ul>
+    </li>
+  {/each}
+</ol>
+
+
+<script lang="ts">
+import type { Course } from "../utils/getCourses";
+
+export let courses: Course[];
+
+</script>
+
+
+<script lang="ts" context="module">
+import type { Load } from "@sveltejs/kit";
+
+export const load: Load = async ({ fetch, url }) => {
+  const data = await fetch("/course.json").then(value => value.json());
+  console.log(data);
+  return {
+    props: {
+      courses: data
+    }
+  };
+};
+</script>
