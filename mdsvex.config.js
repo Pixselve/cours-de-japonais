5349cca38dd87698be4602d1e76172b906403b9d
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-03-02 17:48:42 +0100

    feat: add new layout for course intro

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -3,7 +3,8 @@ import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 	layout: {
-		video: 'src/lib/blog.svelte'
+		video: 'src/lib/blog.svelte',
+		intro: 'src/lib/intro.svelte',
 	},
 
 	smartypants: {
